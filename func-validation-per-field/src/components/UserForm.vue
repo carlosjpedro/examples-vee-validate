df3ca7ddf25720c05f5ad4d610ef67e155c41676
Carlos Pedro <carlospedr@gmail.com>
2020-10-29 20:32:35 +0100

    create vue app for schema validation

--- func-validation-per-field/src/components/UserForm.vue
+++ func-validation-per-field/src/components/UserForm.vue
@@ -1,21 +1,35 @@
 <template>
-  <form @submit.prevent="onSubmit">
+  <v-form @submit="onSubmit" v-slot="{ errors }">
     <label for="username">Username :</label>
-    <input type="text" id="username" name="username" />
-
+    <v-field
+      as="input"
+      type="text"
+      id="username"
+      name="username"
+      :rules="isRequired"
+    />
+    <p>{{ errors.username }}</p>
     <label for="password">Password :</label>
-    <input type="password" id="password" name="password" />
-
+    <v-field
+      as="input"
+      type="password"
+      id="password"
+      name="password"
+      :rules="minLength"
+    />
+    <p>{{ errors.password }}</p>
     <button>Submit</button>
-  </form>
+  </v-form>
+  <ul></ul>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { Form as VForm, Field as VField } from "vee-validate";
 
 export default defineComponent({
+  components: { VForm, VField },
   setup() {
-      
     //fields
     const username = ref<string>();
     const password = ref<string>();
@@ -28,7 +42,23 @@ export default defineComponent({
       });
     };
 
-    return { onSubmit, username, password };
+    //validation returning default message
+    const isRequired = (value: string) => {
+      console.log(`validating value: ${value}`);
+      return !value ? false : value.length;
+    };
+
+    //validation with custom message
+    const minLength = (value: string) => {
+      console.log(`validating value: ${value}`);
+      
+      if (!value || !value.length || value.length < 11) {
+        return "Field must be at leats 10 chars long";
+      }
+      return true;
+    };
+
+    return { onSubmit, isRequired, minLength, username, password };
   },
 });
 </script>
